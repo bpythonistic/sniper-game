97b244bce9ac4357d264df61773d6e115d8a8503
Byron <sixthbaron9@protonmail.com>
2026-02-22 15:27:00 -0500

    Updated ESLint workflow

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,6 @@ import react from "eslint-plugin-react";
 import reacthooks from "eslint-plugin-react-hooks";
 
 export default defineConfig({
-    // extends: [
-    //     "eslint:recommended",
-    //     "plugin:react/recommended",
-    //     "plugin:react-hooks/recommended",
-    //     "plugin:@typescript-eslint/recommended",
-    //     "plugin:import/errors",
-    //     "plugin:import/warnings",
-    //     "plugin:jsx-a11y/recommended",
-    //     "prettier",
-    // ],
-    // parser: "@typescript-eslint/parser",
     plugins: {
         react: react,
         reacthooks: reacthooks,
