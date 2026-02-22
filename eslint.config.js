781b163b5b752049a468d25fff16e2c8f90228d7
Byron <sixthbaron9@protonmail.com>
2026-02-22 15:33:26 -0500

    Workflow corrected

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import react from "eslint-plugin-react";
 import reacthooks from "eslint-plugin-react-hooks";
 
 export default defineConfig({
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     plugins: {
         react: react,
         reacthooks: reacthooks,
