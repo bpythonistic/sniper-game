617653322f6839454ec527738ce7c351e4791665
Byron <sixthbaron9@protonmail.com>
2026-02-22 09:34:28 -0500

    Config file fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "eslint-define-config";
 
 export default defineConfig({
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:jsx-a11y/recommended",
-        "prettier",
-    ],
+    // extends: [
+    //     "eslint:recommended",
+    //     "plugin:react/recommended",
+    //     "plugin:react-hooks/recommended",
+    //     "plugin:@typescript-eslint/recommended",
+    //     "plugin:import/errors",
+    //     "plugin:import/warnings",
+    //     "plugin:jsx-a11y/recommended",
+    //     "prettier",
+    // ],
     parser: "@typescript-eslint/parser",
     plugins: [
         "@typescript-eslint",
