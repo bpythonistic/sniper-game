861f15f27c402224c4715e2c439943d0123f50e0
Byron <sixthbaron9@protonmail.com>
2026-02-27 08:19:13 -0500

    Pre-commit updates

--- back-end/tests/test_main.py
+++ back-end/tests/test_main.py
@@ -66,9 +66,7 @@ class DataModel(BaseModel):
 
 
 @pytest.fixture(scope="function")
-def database_setup(
-    client: TestClient, request: pytest.FixtureRequest
-) -> DataModel:
+def database_setup(client: TestClient, request: pytest.FixtureRequest) -> DataModel:
     """
     Fixture for returning a database state based on the data fixture.
     """
